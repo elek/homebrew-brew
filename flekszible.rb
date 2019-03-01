e59aca31651d1d48dc675a4012ca66f8d0f2b5c3
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-01 08:45:58 +0100

    Brew formula update for flekszible version v1.0.0

--- flekszible.rb
+++ flekszible.rb
@@ -1,9 +1,9 @@
 class Flekszible < Formula
   desc "Composition based kubernetes resource file manager"
   homepage ""
-  url "https://github.com/elek/flekszible/releases/download/v0.3.0/flekszible_0.3.0_Darwin_x86_64.tar.gz"
-  version "0.3.0"
-  sha256 "9f7214c3b3d5073175ba5036ee2147f12832fe86cffb846b9cd6ef2f9d1941b2"
+  url "https://github.com/elek/flekszible/releases/download/v1.0.0/flekszible_1.0.0_Darwin_x86_64.tar.gz"
+  version "1.0.0"
+  sha256 "3587d1e4e0f0d574690120498731cf29a149a057bebdbb1a474d245fa40b1da0"
 
   def install
     bin.install "flekszible"
